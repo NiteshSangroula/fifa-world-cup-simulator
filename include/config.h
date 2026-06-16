abd66430b8b733cda09b951a36d0aeb077c9b8fb
NiteshSangroula <sangroulanitesh123@gmail.com>
2026-06-16 18:24:36 +0545

    config: add default values, define all structs, platform independent with platform macro

---
+++ include/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define STAT_MIN 55
+#define STAT_MAX 99
+#define MAX_PLAYERS 1300
+#define MAX_GKS 200
+#define MAX_TEAMS 48
+#define MAX_GROUPS 12
+#define SQUAD_SIZE 26
+#define GKS_PER_TEAM 3
+#define OUTFIELD_PER_TEAM 23
+#define MAX_MATCHES 200
+#define GOAL_EVENT_DELAY 800 /* milliseconds between goal events */
+#define NAME_LEN 64
+#define GROUP_NAME_LEN 4
+#define DATE_LEN 12
+#define MAX_GOAL_EVENTS 20
+
+#endif

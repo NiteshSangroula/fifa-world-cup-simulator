516c62f22e78e63381d586f4937c4d64e185cc8f
NiteshSangroula <sangroulanitesh123@gmail.com>
2026-06-22 20:38:46 +0545

    use nanosleep instead of usleep

--- include/platform.h
+++ include/platform.h
@@ -1,12 +1,23 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+// Define POSIX feature test macro before any includes
+#define _POSIX_C_SOURCE 199309L // For nanosleep()
+
 #ifdef _WIN32
 #include <windows.h>
 #define sleep_ms(ms) Sleep(ms)
 #else
-#include <unistd.h>
-#define sleep_ms(ms) usleep((ms) * 1000)
+#include <time.h>
+
+// Use nanosleep instead of usleep (more modern)
+static inline void sleep_ms(int ms)
+{
+    struct timespec ts;
+    ts.tv_sec  = ms / 1000;
+    ts.tv_nsec = (ms % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+}
 #endif
 
 #endif
